470bacd7b0fc28a5d5df812940831f15d242a397
NguyenHiep <hiepghast23@gmail.com>
2023-10-01 16:53:41 +0700

    update

--- core/views.py
+++ core/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+
+from .forms import FormSignUp
+
+def signup(request):
+    if request.method == "POST":
+        form = FormSignUp(request.POST)
+        
+        if form.is_valid():
+            login(request, user=form.save())
+            
+            return redirect("front_page")
+        
+    else:
+        form = FormSignUp()
+        
+    context = {
+        "form" : form
+    }
+    
+    return render(request, "core/signup.html", context)
 
 # Create your views here.
 def front_page(request):
-    return render(request, "core/base.html")
+    return render(request, "core/front_page.html")
+
+
